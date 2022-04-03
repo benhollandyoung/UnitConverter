b06466ca523d93f7aaad54074ec550722bebc3d2
Ben Holland Young <benjaminhollandyoung@gmail.com>
2022-04-03 21:09:24 +0100

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Ben Holland on 03/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
